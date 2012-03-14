afeb981519d7ecbf74c4efb91c49a9e9487d709e
Warren Voelkl <warren_voelkl@hotmail.com>
2012-03-14 16:32:47 -0700

    modified git ignore

--- src/mainTestMap.c
+++ src/mainTestMap.c
@@ -1,6 +1,14 @@
 #include "mapHash.h"
+#include <stdio.h>
 
 int main() {
-	mapAdd
+	PMAP map = malloc(sizeof(MAP));
+    printf("a\n");
+    map->clientPort = 2;
+    mapAdd(map);
+    printf("b\n");
+    PMAP b = mapFind(2);
+    printf("address %d\n",b);
+//	printf("map port %d\n", b->clientPort);
 	return 0;
 }
