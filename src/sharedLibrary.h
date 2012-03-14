b01234a208bc247d8a8973b29fcbf25449d0ece5
Luke Queenan <lukequeenan@gmail.com>
2012-03-14 16:12:26 -0700

    Removed linked list and formatted some code

--- src/sharedLibrary.h
+++ src/sharedLibrary.h
@@ -2,15 +2,7 @@
 #define SHARED_LIBRARY_H
 
 /* Defines */
-struct list
-{
-    int externPort;
-    char ip[16];
-    int internPort;
-    struct list* next;
-};
 
-typedef struct list rule;
 
 /* Function Prototypes */
 void systemFatal(const char* message);
