2a236ca2d07422989e615428e0e5658ea42d13ad
Warren Voelkl <warrenvoelkl@gmail.com>
2012-03-16 13:22:28 -0700

    added port test to mainTest

--- src/mainTestMap.c
+++ src/mainTestMap.c
@@ -2,6 +2,9 @@
 #include "clientHash.h"
 #include "serverHash.h"
 #include "sharedLibrary.h"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 int main() {
     printf("----Testing adding hash entry to both tables----\n");
@@ -18,5 +21,17 @@ int main() {
     printf("findServer should return value 0 result: %d\n", serverFind(a));
     clientDelete(1,1);
     printf("findclient should return value 0 result: %d\n", clientFind(1,1));
+ 
+    unsigned long x = inet_addr("192.168.0.1");
+    unsigned int y = inet_addr("192.168.0.1");
+    printf("unsigned int:  %lu should match unsigned long: %d\n", y, x);
+
+
+    unsigned int p = htons(a);
+    unsigned short q = htons(a);
+
+    printf("network ports unsigned int: %d, unsigned short %d\n", p, q);
+
     return 0;
+
 }
