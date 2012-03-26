b4f70e79fce52be79ff1aecdef0e8476ffe16b9f
Warren Voelkl <warrenvoelkl@gmail.com>
2012-03-26 10:25:42 -0700

    Made Unit Tests presentable

--- src/mainTestMap.c
+++ src/mainTestMap.c
@@ -1,57 +1,59 @@
 #include <stdio.h>
-#include "clientHash.h"
-#include "serverHash.h"
-#include "ruleHash.h"
-#include "sharedLibrary.h"
+#include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#include "serverHash.h"
+#include "ruleHash.h"
+#include "clientHash.h"
+
+#include "sharedLibrary.h"
+
 int main() {
+
     printf("----Testing adding hash entry to both tables----\n");
     unsigned int a = randomSourcePort();
-    printf("serverPort: %d\n", a);
+
     clientAdd(1,1,a);
-    serverAdd(a,1,1);
+    serverAdd(a,1,1,1);
     PCLIENT b = clientFind(1,1);
-    printf("clientFind should return serverPort %d: %d\n", a, b->serverPort);
+    printf("clientFind should return serverPort %d: \t\t%d\n", a, b->serverPort);
     PSERVER c = serverFind(b->serverPort);
-    printf("serverFind should return 1,1: %d,%d\n", c->clientIp, c->clientPort);
-    printf("----Deleting hash entries from both tables-----\n");
+    printf("serverFind should return 1,1: \t\t\t\t%d,%d\n", c->clientIp, c->clientPort);
+    printf("\n----Deleting hash entries from both tables-----\n");
     serverDelete(a); 
-    printf("findServer should return value 0 result: %d\n", serverFind(a));
+    printf("findServer should return value 0 result: \t\t%d\n", serverFind(a));
     clientDelete(1,1);
-    printf("findclient should return value 0 result: %d\n", clientFind(1,1));
- 
+    printf("findclient should return value 0 result: \t\t%d\n", clientFind(1,1));
+
+    printf("\n----Testing RuleHash Map-----\n");
     unsigned long x = inet_addr("192.168.0.1");
     unsigned int y = inet_addr("192.168.0.1");
-    printf("unsigned int:  %lu should match unsigned long: %d\n", y, x);
+    printf("unsigned int:  %lu should match unsigned long: \t%d\n", y, x);
     unsigned int p = htons(a);
     unsigned short q = htons(a);
-    printf("network ports unsigned int: %d, unsigned short %d\n", p, q);
+    printf("network ports unsigned int: %d, unsigned short \t%d\n", p, q);
     unsigned int z = inet_addr("192.168.0.2");
     unsigned int r = htons(22);
-    unsigned int s = addRuleToMaps(y,r);
-    printf("server port: %d\n", s);
+    unsigned int s = addRuleToMaps(y,r,2);
     b = clientFind(y,r);
-    printf("clientFind should return serverPort %d: %d\n", s, b->serverPort);
+    printf("clientFind should return serverPort %d: \t\t%d\n", s, b->serverPort);
     c = serverFind(s);
-    /*printf("serverFind should return %d,%d: %d,%d\n", y, r, c->clientIp, c->clientPort);
-    printf("----Deleting hash entries from both tables using deleteRuleFromMaps and just serverPort-----\n");
-    deleteRuleFromMap(0,0,s);
-    printf("findServer should return value 0 result: %d\n", serverFind(s));
-    printf("findclient should return value 0 result: %d\n", clientFind(y, r));*/
     unsigned int clientIp;
     unsigned short clientPort;
-    if (srvFind(s, &clientIp, &clientPort)) {
-        printf("Client IP: %d, client Port %d\n", clientIp, clientPort);
+    unsigned short clientSourcePort;
+    printf("\n----Testing external functions in sharedLibrary.c-----\n");
+    if (srvFind(s, &clientIp, &clientPort, &clientSourcePort)) {
+        printf("Client IP: \t%d, \tClient Port: %d\n", clientIp, clientPort);
     } else {
         printf("Server Rule Not found\n");
 
     }
     unsigned short serverPort;
     if (cliFind(y, r, &serverPort)) {
-        printf("server port: %d\n", serverPort);
+        printf("server port: \t%d\n", serverPort);
     } else {
         printf("client Rule not Found\n");
     }
@@ -59,22 +61,35 @@ int main() {
     unsigned int serverDestIp = z;
     unsigned int serverDestPort = htons(9999);
     unsigned int clientDestPort = htons(22222);
-    rlAdd(clientDestPort, serverDestPort, serverDestIp);
+    char *str1 = malloc(16);
+    snprintf(str1, 16, "192.168.0.194");
+    char *str2 = malloc(16);
+    snprintf(str2, 16, "192.168.0.196");
+    rlAdd(clientDestPort, serverDestPort, serverDestIp,str1,str2);
     unsigned int ddd;
     unsigned short eee;
     rlFind(clientDestPort, &eee, &ddd);
-    printf("rlFind should return value %d: %d\n",serverDestPort, eee);
-
-    printf("%s\n", rlToStr());
-    rlAdd(htons(22223), serverDestPort, serverDestIp);
-    rlAdd(htons(22224), serverDestPort, serverDestIp);
-    rlAdd(htons(22225), serverDestPort, serverDestIp);
-    printf("%s\n", rlToStr());
+    printf("rlFind should return value %d: \t\t\t%d\n",serverDestPort, eee);
 
 
-
-
-
+    str1 = malloc(16);
+    snprintf(str1, 16, "192.168.0.195");
+    str2 = malloc(16);
+    snprintf(str2, 16, "192.168.0.196");
+    rlAdd(htons(22223), serverDestPort, serverDestIp,str1,str2);
+    str1 = malloc(16);
+    snprintf(str1, 16, "192.168.0.197");
+    str2 = malloc(16);
+    snprintf(str2, 16, "192.168.0.196");
+    rlAdd(htons(22224), serverDestPort, serverDestIp,str1,str2);
+    str1 = malloc(16);
+    snprintf(str1, 16, "192.168.0.198");
+    str2 = malloc(16);
+    snprintf(str2, 16, "192.168.0.196");
+    rlAdd(htons(22225), serverDestPort, serverDestIp,str1,str2);
+    printf("\n----Testing filter generation in toString functions for ruleHash-----\n");
+    printf("%s\n", rlExternToStr());
+    printf("%s\n", rlInternToStr());
     return 0;
 
 }
